3fa0e369ce1434cc5b4f8cbd2db0e05629f49e44
Marcin Warpechowski <warpech@gmail.com>
2013-06-17 09:47:27 +0200

    add Angular UI Handsontable

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -15,6 +15,38 @@ Site.config(function ($routeProvider, $locationProvider) {
 /**
  * AppController
  */
-function AppController ($scope) {
+function AppController($scope, $rootScope) {
   $scope.mainFile = window.location.pathname;
+
+  $scope.username = "Marcin";
+
+  $scope.subpage = {
+    people: [
+      {id: 1, name: {first: 'Joe', last: 'Fabiano'}, gender: 'Male', age: 21, languages: {english: 'Yes', spanish: 'Yes', french: 'No'}, salary: 2000},
+      {id: 2, name: {first: 'Fred', last: 'Wecler'}, gender: 'Male', age: 25, languages: {english: 'Yes', spanish: 'No', french: 'No'}, salary: 2500},
+      {id: 3, name: {first: 'Steve', last: 'Wilson'}, gender: 'Male', age: 32, languages: {english: 'Yes', spanish: 'No', french: 'No'}, salary: 1700},
+      {id: 4, name: {first: 'Maria', last: 'Fernandez'}, gender: 'Female', age: 27, languages: {english: 'No', spanish: 'Yes', french: 'Yes'}, salary: 3000},
+      {id: 5, name: {first: 'Pierre', last: 'Barbault'}, gender: 'Male', age: 46, languages: {english: 'Yes', spanish: 'No', french: 'Yes'}, salary: 1450},
+      {id: 6, name: {first: 'Nancy', last: 'Moore'}, gender: 'Female', age: 34, languages: {english: 'Yes', spanish: 'No', french: 'No'}, salary: 2300},
+      {id: 7, name: {first: 'Barbara', last: 'MacDonald'}, gender: 'Female', age: 19, languages: {english: 'Yes', spanish: 'No', french: 'No'}, salary: 1900},
+      {id: 8, name: {first: 'Wilma', last: 'Williams'}, gender: 'Female', age: 33, languages: {english: 'Yes', spanish: 'Yes', french: 'Yes'}, salary: 2400},
+      {id: 9, name: {first: 'Sasha', last: 'Silver'}, gender: 'Male', age: 27, languages: {english: 'Yes', spanish: 'No', french: 'Yes'}, salary: 2110},
+      {id: 10, name: {first: 'Don', last: 'Pérignon'}, gender: 'Male', age: 42, languages: {english: 'No', spanish: 'No', french: 'Yes'}, salary: 2090},
+      {id: 11, name: {first: 'Aaron', last: 'Kinley'}, gender: 'Female', age: 33, languages: {english: 'Yes', spanish: 'Yes', french: 'Yes'}, salary: 2799}
+    ],
+    settings: {
+      isEmptyRow: function (r) {
+        var val;
+        //c === 1 to ignore id column
+        for (var c = 1, clen = this.countCols(); c < clen; c++) {
+          val = this.getDataAtCell(r, c);
+          if (val !== '' && val !== null && typeof val !== 'undefined') {
+            return false;
+          }
+        }
+        return true;
+      }
+    },
+    html: "Loading Ajax..."
+  }
 }
