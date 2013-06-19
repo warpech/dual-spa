c88224c10f1f39e70c8020723de5fd083b6af05d
Marcin Warpechowski <warpech@gmail.com>
2013-06-19 10:24:18 +0200

    move mock server to a separate file

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -52,33 +52,5 @@ function AppController($scope, $rootScope) {
 
   //simulate server
 
-  var current = 0;
-  var names = ["Fred", "Wilma", "Pebbles", "Dino", "Barney", "Betty", "Bamm-Bamm"];
-
-  function changeUsername() {
-    var patch = [
-      {op: "replace", path: "/username", value: names[current] }
-    ];
-
-    for (var i = 0, ilen = $scope.subpage.people.length; i < ilen; i++) {
-      if ($scope.subpage.people[i].name.first) {
-        patch.push({op: "replace", path: "/subpage/people/" + i + "/name/first", value: names[current] });
-      }
-    }
-
-    jsonpatch.apply($scope, patch);
-    if (!$scope.$$phase) {
-      $scope.$apply();
-    }
-  }
-
-  setInterval(function () {
-    current++;
-    if (current === names.length) {
-      current = 0;
-    }
-    changeUsername()
-  }, 2000);
-
-  changeUsername()
+  mockServer($scope);
 }
