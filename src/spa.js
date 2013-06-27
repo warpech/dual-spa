8850d5bab6fd612ba84d7f6cadea301dd95d03cd
Marcin Warpechowski <warpech@gmail.com>
2013-06-28 01:18:04 +0200

    move JSON model and view operations to spa.js

--- src/spa.js
+++ src/spa.js
@@ -1,5 +1,7 @@
 (function (global) {
 
+  var obj;
+
   /**
    * Defines a connection to a remove URL, returns callback to a object that is persistent between browser and server
    * @param remoteUrl
@@ -8,7 +10,8 @@
   function SPA(remoteUrl, callback) {
     var oReq = new XMLHttpRequest();
     oReq.addEventListener('load', function (event) {
-      callback(JSON.parse(event.target.responseText));
+      obj = JSON.parse(event.target.responseText);
+      callback(obj);
     }, false);
     oReq.open("GET", remoteUrl, true);
     oReq.send();
@@ -21,12 +24,36 @@
   }
 
   function clickHandler(event) {
-    if (event.target.nodeName === 'A' && isApplicationLink(event.target)) {
+    var target = event.target;
+    if (window.spaExternalLink) {
+      target = window.spaExternalLink;
+      window.spaExternalLink = null;
+    }
+    if (target.href && isApplicationLink(target)) {
       event.preventDefault();
-      history.pushState(null, null, event.target.href);
-      //parseRoute();
+      history.pushState(null, null, target.href);
+      parseRoute(target.href);
     }
   }
 
-  global.SPA = SPA;
+  function parseRoute(href) {
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener('load', function (event) {
+      var patches = JSON.parse(event.target.responseText);
+      jsonpatch.apply(obj, patches);
+    }, false);
+    oReq.open("GET", href, true);
+    //oReq.setRequestHeader('Accept-Encoding', 'application/json-patch'); //cannot do this in Chrome - http://news.anarchy46.net/2012/06/refused-to-set-unsafe-header.html
+    oReq.setRequestHeader('Content-Type', 'application/json-patch'); //cannot do this in Chrome - http://news.anarchy46.net/2012/06/refused-to-set-unsafe-header.html
+    oReq.send();
+  }
+
+  global.SPA = {
+    init: SPA,
+    catchExternaLink: function (element) {
+      element.addEventListener("click", function (event) {
+        window.spaExternalLink = event.target;
+      });
+    }
+  };
 })(window);
--- wc/components/x-viewmodel/mockServer.js
+++ wc/components/x-viewmodel/mockServer.js
@@ -1,28 +1,116 @@
-function mockServer(scope) {
-  var current = 0;
-  var names = ["Fred", "Wilma", "Pebbles", "Dino", "Barney", "Betty", "Bamm-Bamm"];
+var full = {
+  username: "Marcin",
+  subpage: {
+    people: [
+      {id: 1, name: {first: 'Joe', last: 'Fabiano'}, gender: 'Male', age: 21, languages: {english: 'Yes', spanish: 'Yes', french: 'No'}, salary: 2000},
+      {id: 2, name: {first: 'Fred', last: 'Wecler'}, gender: 'Male', age: 25, languages: {english: 'Yes', spanish: 'No', french: 'No'}, salary: 2500},
+      {id: 3, name: {first: 'Steve', last: 'Wilson'}, gender: 'Male', age: 32, languages: {english: 'Yes', spanish: 'No', french: 'No'}, salary: 1700},
+      {id: 4, name: {first: 'Maria', last: 'Fernandez'}, gender: 'Female', age: 27, languages: {english: 'No', spanish: 'Yes', french: 'Yes'}, salary: 3000},
+      {id: 5, name: {first: 'Pierre', last: 'Barbault'}, gender: 'Male', age: 46, languages: {english: 'Yes', spanish: 'No', french: 'Yes'}, salary: 1450},
+      {id: 6, name: {first: 'Nancy', last: 'Moore'}, gender: 'Female', age: 34, languages: {english: 'Yes', spanish: 'No', french: 'No'}, salary: 2300},
+      {id: 7, name: {first: 'Barbara', last: 'MacDonald'}, gender: 'Female', age: 19, languages: {english: 'Yes', spanish: 'No', french: 'No'}, salary: 1900},
+      {id: 8, name: {first: 'Wilma', last: 'Williams'}, gender: 'Female', age: 33, languages: {english: 'Yes', spanish: 'Yes', french: 'Yes'}, salary: 2400},
+      {id: 9, name: {first: 'Sasha', last: 'Silver'}, gender: 'Male', age: 27, languages: {english: 'Yes', spanish: 'No', french: 'Yes'}, salary: 2110},
+      {id: 10, name: {first: 'Don', last: 'Pérignon'}, gender: 'Male', age: 42, languages: {english: 'No', spanish: 'No', french: 'Yes'}, salary: 2090},
+      {id: 11, name: {first: 'Aaron', last: 'Kinley'}, gender: 'Female', age: 33, languages: {english: 'Yes', spanish: 'Yes', french: 'Yes'}, salary: 2799}
+    ],
+    settings: {
 
-  function changeUsername() {
-    var patch = [
-      {op: "replace", path: "/username", value: names[current] }
-    ];
+    },
+    html: "@partials/page_1.html"
+  }
+};
 
-    for (var i = 0, ilen = scope.subpage.people.length; i < ilen; i++) {
-      if (scope.subpage.people[i].name.first) {
-        patch.push({op: "replace", path: "/subpage/people/" + i + "/name/first", value: names[current] });
+////
+
+var current = 0;
+var names = ["Fred", "Wilma", "Pebbles", "Dino", "Barney", "Betty", "Bamm-Bamm"];
+
+function changeUsername() {
+  var patches = [
+    {op: "replace", path: "/username", value: names[current] }
+  ];
+
+  for (var i = 0, ilen = full.subpage.people.length; i < ilen; i++) {
+    if (full.subpage.people[i].name.first) {
+      patches.push({op: "replace", path: "/subpage/people/" + i + "/name/first", value: names[current] });
+    }
+  }
+
+  current++;
+  if (current === names.length) {
+    current = 0;
+  }
+
+  return patches;
+}
+
+
+////
+
+
+var server = sinon.fakeServer.create();
+server.autoRespond = true;
+
+server.xhr.useFilters = true;
+server.xhr.addFilter(function (method, url) {
+  if (url.indexOf('partial') > -1) {
+    return true;
+  }
+});
+
+var lastUrl = window.location.href;
+
+if (lastUrl.indexOf('page_1') > -1) {
+  full.subpage.html = '@partials/page_1.html';
+}
+else if (lastUrl.indexOf('page_2') > -1) {
+  full.subpage.html = '@partials/page_2.html';
+}
+
+server.respondWith(function (xhr) {
+  var patches = changeUsername();
+  var partial;
+  if (xhr.requestHeaders['Content-Type'] == 'application/json-patch') {
+    if (xhr.url !== lastUrl) {
+      lastUrl = xhr.url;
+      if (lastUrl.indexOf('page_1') > -1) {
+        patches.push({op: 'replace', path: '/subpage/html', value: '@partials/page_1.html'});
+//        patches.push({op: 'replace', path: '/subpage/html', value: '<h1>This is Page 1 for {{ model.username }}</h1>'});
+//        partial = 'partials/page_1.html';
+      }
+      else if (lastUrl.indexOf('page_2') > -1) {
+        patches.push({op: 'replace', path: '/subpage/html', value: '@partials/page_2.html'});
+//        patches.push({op: 'replace', path: '/subpage/html', value: '<h1>This is Page 2 for {{ model.username }}</h1>'});
+//        partial = 'partials/page_2.html';
       }
     }
 
-    jsonpatch.apply(scope, patch);
-  }
-
-  setInterval(function () {
-    current++;
-    if (current === names.length) {
-      current = 0;
+    if (partial) {
+      /*var oReq = new XMLHttpRequest();
+       var that = this;
+       oReq.onload = function (event) {
+       patches.push({op: 'replace', path: '/subpage/html', value: event.target.responseText});
+       xhr.respond(200, 'application/json-patch', JSON.stringify(patches));
+       };
+       oReq.open("GET", partial, true);
+       oReq.send();*/
     }
-    changeUsername()
-  }, 2000);
+    else {
+      xhr.respond(200, 'application/json-patch', JSON.stringify(patches));
+    }
+  }
+  else {
+    xhr.respond(200, 'application/json', JSON.stringify(full));
+  }
+});
 
-  changeUsername()
-}
+server.respond();
+
+/*setInterval(function () {
+ current++;
+ if (current === names.length) {
+ current = 0;
+ }
+ changeUsername()
+ }, 2000);*/
